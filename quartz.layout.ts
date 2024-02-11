6c51772d4d14ecc6208eb5434acd3ccdd4adbef5
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2024-02-11 14:44:27 +0000

    Remove 'last modified' line

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,7 +15,6 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
