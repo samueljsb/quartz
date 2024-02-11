73faf9a1e9b2352c174b55c8a7788e0962ef709a
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2024-02-11 14:42:47 +0000

    Set title and URLs

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,14 +3,14 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🛡️ Mithanaepedia",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mithanaepedia.samueljsb.co.uk",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
